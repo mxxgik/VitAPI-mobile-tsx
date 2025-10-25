53e8aa6b5dbf8f1eb2b1a7be8900871149927510
mxxgik <andres.chaparro7@hotmail.com>
2025-10-25 08:35:03 -0500

    Added better linting configuration for the project

--- eslint.config.js
+++ eslint.config.js
@@ -6,5 +6,11 @@ module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+    rules: {
+      'react-hooks/exhaustive-deps': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'react/display-name': 'off', 
+    },
   },
 ]);
